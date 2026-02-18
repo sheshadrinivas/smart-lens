817234c770da6816f49d7ffe0562ce0e9a0bbd8b
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2026-02-18 20:28:18 +0530

    cli

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 #!/usr/bin/env node
 import sharp from "sharp";
-
+import chalk from "chalk";
 const args = process.argv.slice(2);
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function stringPrint(string_, time_, color_) {
+  for (let char of string_) {
+    process.stdout.write(chalk[color_](char));
+    await sleep(time_);
+  }
+  console.log();
+}
 if (args.length === 0) {
   console.log("⚠️ No images provided.");
   process.exit(1);
 }
+console.log("");
 
-console.log("Orbital Vision System (OVS)");
-console.log("Processing images:");
+await stringPrint("Orbital Vision System (OVS)", 40, "green");
+console.log("");
+console.log(chalk.italic.green("Processing images ["));
+console.log("");
 for (let a = 1; a < args.length; a++) {
-  console.log(args[a]);
+  // console.log(chalk.cyan(args[a]));
+  await stringPrint(args[a], 10, "cyan");
+  console.log("");
 }
+console.log(chalk.italic.green("]"));
+console.log("");
+await stringPrint("Initializing...", 40, "blue");
+await stringPrint("Loading dependencies...", 40, "yellow");
+await stringPrint("Loading images into memory...", 40, "yellow");
 
 const total_array = [];
 
@@ -54,7 +75,7 @@ class Fingerprint {
     return (crc ^ -1) >>> 0;
   }
 }
-
+await stringPrint("Initializing image processing...", 25, "yellow");
 for (let i = 1; i < args.length; i++) {
   const test = new Fingerprint(args[i]);
   total_array.push(await test.generate());
@@ -62,6 +83,7 @@ for (let i = 1; i < args.length; i++) {
 const test = new Fingerprint(args[0]);
 const testArray = await test.generate();
 const match = [];
+await stringPrint("Initializing image matching...", 25, "yellow");
 for (let i = 0; i < total_array.length; i++) {
   let weight = 0;
   const array_1 = total_array[i];
@@ -73,13 +95,15 @@ for (let i = 0; i < total_array.length; i++) {
       weight++;
     }
   }
-
-  console.log("weight:", (weight / testArray.length) * 100);
+  console.log("");
+  await stringPrint("weight: " + (weight / testArray.length) * 100, 40, "red");
   match.push((weight / testArray.length) * 100);
 }
+console.log("");
 
 Math.max(...match);
 const maxValue = Math.max(...match);
 const index = match.indexOf(maxValue);
-
-console.log("best match:", args[index + 1]);
+console.log("");
+await stringPrint("best match: ", 20, "red");
+console.log(chalk.bgBlackBright.italic.white(args[index + 1]));
